3d21533e5c674d640f2d122180eceb25092a1e99
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-05-07 16:37:58 -0500

    New Updates

---
+++ script.js
@@ -0,0 +1,23 @@
+const thankYou = function thank() {
+    alert("THANK YOU R2H!");
+
+}
+
+
+
+function getNoti(){
+    document.getElementById("noti").innerHTML = "* SUBSCRIBED *"
+}
+
+
+
+
+    
+
+
+
+
+
+
+
+
